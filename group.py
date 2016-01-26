fd1514b09c52338df856f8435696ad63fb4e1784
Matej Dujava <mdujava@gmail.com>
2016-01-26 19:02:24 +0100

    add basic classes.
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

---
+++ group.py
@@ -0,0 +1,5 @@
+class group(object):
+    def __init__(self, name, nameday_atr, birthday_atr):
+        self.name = name
+        self.nameday_atr = nameday_atr
+        self.birthday_atr = birthday_atr
---
+++ person.py
@@ -0,0 +1,10 @@
+class Person(object):
+    def __init__(self, first_name, second_name, birthday_date, nameday_date, mail, tel_number, facebook, group = []):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.birthday_date = birthday_date
+        self.nameday_date = nameday_date
+        self.mail = mail
+        self.tel_number = tel_number
+        self.facebook = facebook
+        self.group = group
