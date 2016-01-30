d930e62268afc77934821bae131c4433e340996c
Matej Dujava <mdujava@gmail.com>
2016-01-30 10:21:49 +0100

    Working shell.
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

--- shell.py
+++ shell.py
@@ -1,4 +1,5 @@
 from person import Person
+import readline
 import re
 
 
@@ -6,11 +7,24 @@ def go(db):
     global status
     while status == 1:
         inputText = input("command> ")
-        res = {}
+        resArray = {}
         for i in commands:
             res = re.search("^" + inputText + ".*", i)
+            resArray[i] = res
 
-            #commands[i](db)
+        candidate = []
+        for i in resArray:
+            if resArray[i] != None:
+                candidate.append(i)
+
+        if len(candidate) == 1:
+            commands[candidate[0]](db)
+        elif len(candidate) > 1:
+            print("do you mean:")
+            for can in candidate:
+                print("\t", can)
+        else:
+            print("use h/help for help.")
 
 def helpMe(db):
     print("help:")
@@ -45,6 +59,7 @@ status = 1
 commands = {"help": helpMe,
             "add": add,
             "quit": quit,
+            "exit": quit,
             "list": showDb}
 commandsHelp = {"help": "help for command help",
                 "add": "help for command add",
