ea1393882d22d463fa74c09f91e844ec2f1ba254
Matej Dujava <mdujava@gmail.com>
2016-01-31 13:23:00 +0100

    Add argparse.
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from personDb import PersonDb
 from shell import Shell
+import argparse
+import sys
 
 
-def main():
-    shell = Shell("skuska")
-    shell.go()
+def main(shell):
+    if shell:
+        shell = Shell("skuska")
+        shell.go()
+    else:
+        pass
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='BirthdayNotify')
+    parser.add_argument("--shell", action='store_true', default=False)
+
+    result = parser.parse_args()
+
+    main(result.shell)
