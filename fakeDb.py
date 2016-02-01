4e154e82b0f47874ba194f97cf456e501844ed14
Matej Dujava <mdujava@gmail.com>
2016-02-01 22:02:46 +0100

    Half way.
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

--- fakeDb.py
+++ fakeDb.py
@@ -5,18 +5,31 @@ from groupDb import GroupDb
 
 class FakeDb(object):
     def __init__(self, dbName):
-        self.dbName = self.fileExtension(dbName)
-        self.personDb = PersonDb(self.dbName, False)
-        self.grouDb = GrouDb(self.dbName, False)
+        self.setDbNames(dbName)
+        self.personDb = PersonDb(self.personDbName, False)
+        self.grouDb = GroupDb(self.groupDbName, False)
 
-    def save(self, fileName = 'database'):
+    def save(self, fileName = None):
+        if fileName == None:
+            fileName = self.personDbName
+#change to _save
         self.personDb.save(fileName) 
         self.grouDb.save(fileName) 
 
-    def load(self, fileName = 'database'):
+    def load(self, fileName = None):
+        if fileName == None:
+            fileName = self.groupDbName
+#change to _save
+        self.personDb.load(fileName)
+        self.grouDb.load(fileName)
 
     def fileExtension(fileName):
         result = fileName.strip()
-        if '.json' not in result:
-            result += '.json'
+        if '.db' not in result:
+            result += '.db'
         return result
+
+    def setDbNames(self, dbName):
+        self.personDbName = FakeDb.fileExtension(dbName + ".p")
+        self.groupDbName = FakeDb.fileExtension(dbName + ".g")
+
--- personDb.py
+++ personDb.py
@@ -6,6 +6,7 @@ class PersonDb(object):
     def __init__(self, dbName, autoload = True):
         self.dbName = dbName
         self.db = None
+        self.isChanged = False
         if autoload:
             self.setup()
 
@@ -21,28 +22,31 @@ class PersonDb(object):
         self.groups = tmp
 
     def add(self, person):
+        self.isChanged = True
         self.db.append(person)
 
-    def save(data, fileName = None):
-        if fileName == None:
-            fileName = self.dbName
-        filen = PersonDb.fileExtension(fileName)
-        with open(filen,"wb") as pickleOut:
-            pickle.dump(data, pickleOut)
+    def edit(self, person, newPerson):
+        self.isChanged = True
+        self.db.index(person)
+        self.db[index] = newPerson
 
-    def load(fileName = None):
+    def save(self, data, fileName = None):
         if fileName == None:
             fileName = self.dbName
-        filen = PersonDb.fileExtension(fileName)
+        with open(fileName,"wb") as pickleOut:
+            pickle.dump(data, pickleOut)
+            self.dbName = fileName
+            self.isChanged = False
+
+    def load(self, fileName = None):
+        if fileName == None:
+            fileName = self.dbName
+        self.dbName = fileName
+        self.isChanged = False
         try:
-            with open(filen, "rb") as pickleIn:
+            with open(fileName, "rb") as pickleIn:
                 data = pickle.load(pickleIn)
             return data
         except FileNotFoundError:
             return []
 
-    def fileExtension(fileName):
-        result = fileName.strip()
-        if '.json' not in result:
-            result += '.json'
-        return result
