39386a19e25be0cade792fe35f6b3f7bfd978ca2
Matej Dujava <mdujava@gmail.com>
2016-02-07 23:45:39 +0100

    Fix parsing.
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

--- birthdayNotify/main.py
+++ birthdayNotify/main.py
@@ -3,18 +3,20 @@ from birthdayNotify.shell import Shell
 import argparse
 import sys
 
-
-def main(shell = True):
+def some(shell):
     if shell:
         shell = Shell("skuska")
         shell.go()
     else:
         pass
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='BirthdayNotify')
     parser.add_argument("--shell", action='store_true', default=False)
 
     result = parser.parse_args()
 
-    main(result.shell)
+    some(result.shell)
+
+if __name__ == '__main__':
+    main()
